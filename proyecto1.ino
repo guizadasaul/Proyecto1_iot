5b73faa9d2fb5120ae9febc069366ec7c4315db9
guizadasaul <saulguizada@Sauls-MacBook-Pro.local>
2025-03-17 17:15:04 -0400

    refactor: codigo final sin outputs de prueba

--- proyecto1.ino
+++ proyecto1.ino
@@ -7,17 +7,14 @@ Led leds[] = {Led(17), Led(16), Led(4), Led(0), Led(2), Led(15)};
 UltrasonicSensor sensor(5, 18);
 
 int activeLedIndex = -1;
-int medicionCount = 0;
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("Inicializando..."); // Mensaje de prueba
     sensor.init();
     
     for (int i = 0; i < 6; i++) {
         leds[i].off();
     }
-    // delay(100);
 }
 
 void loop() {
@@ -33,11 +30,6 @@ void loop() {
         
         float distance = sensor.getDistance();
 
-        medicionCount++;
-        Serial.print("Medición #");
-        Serial.print(medicionCount);
-        Serial.print(", Distancia Real: ");
-        Serial.print("10 cm");
         Serial.print(", Distancia Medida: ");
         Serial.print(distance);
         Serial.println(" cm");
